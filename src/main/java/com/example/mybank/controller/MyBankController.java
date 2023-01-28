89a16bdbc75fd2e4312273b4d131858180170fde
Jameel Kaisar <65322473+JameelKaisar@users.noreply.github.com>
2023-01-28 19:56:22 +0000

    Add Hello World Template

--- src/main/java/com/example/mybank/controller/MyBankController.java
+++ src/main/java/com/example/mybank/controller/MyBankController.java
@@ -1,14 +1,15 @@
 package com.example.mybank.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MyBankController {
 	
 	@GetMapping(path="/")
 	public String sayHello() {
-		return "Hello from My Bank Application";
+		return "hello";
 	}
 
 }
