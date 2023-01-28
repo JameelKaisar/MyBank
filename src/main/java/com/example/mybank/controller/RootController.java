1cdda09c407d42be49d1594fbeb6d3624392c42f
Jameel Kaisar <65322473+JameelKaisar@users.noreply.github.com>
2023-01-28 20:02:33 +0000

    Add Root Controller

--- src/main/java/com/example/mybank/controller/RootController.java
+++ src/main/java/com/example/mybank/controller/RootController.java
@@ -2,14 +2,13 @@ package com.example.mybank.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class MyBankController {
+public class RootController {
 	
 	@GetMapping(path="/")
-	public String sayHello() {
-		return "hello";
+	public String root() {
+		return "redirect:/login";
 	}
 
 }
